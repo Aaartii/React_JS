d83be3326387ea67ff371792fd4fe2df5f57fbb0
Aarti <aarti89306@gmail.com>
2024-10-17 13:00:33 +0530

    Initial commit

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './card.css'
+import UserIcon from '../UserIcon';
+import { LuMoreHorizontal } from 'react-icons/lu';
+import { Ticket, User } from '../../interfaces';
+import { getStatusIcon } from '../../utils/helper';
+
+
+import Urgent_grey from '../Card/urgent_priority_grey.svg';
+
+function Card({ ticket, userData, hideStatusIcon, hideProfileIcon }: { ticket: Ticket, userData: User, hideStatusIcon: boolean, hideProfileIcon: boolean }) {
+  return (
+    <div className='card'>
+      <div className='top-container'>
+        <div className='ticket-id'>{ticket.id}</div>
+        {hideProfileIcon ? null : <UserIcon name={userData.name} available={userData.available} />}
+      </div>
+      <div className='middle-container'>
+        {hideStatusIcon ? null : getStatusIcon(ticket.status)}
+        <div className='title'>{ticket.title}</div>
+      </div>
+      <div className='bottom-container'>
+        <div className='more-icon-container'>
+          {hideProfileIcon ? null : (<img src={Urgent_grey} alt="User Icon" className="custom-user-icon" />  // Use custom image for the user icon
+)}
+        </div>
+        {ticket.tag.map((t: string) => (
+          <div key={t} className='tag-container'>
+            <div className='tag-icon'></div>
+            <div className='tag-text'>{t}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Card;
